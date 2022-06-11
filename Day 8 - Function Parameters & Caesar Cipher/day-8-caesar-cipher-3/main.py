4c607e4c3b2ec07c906336cf1323526e70ab02cf
Paloma Corvera <77248450+palomacorvera@users.noreply.github.com>
2022-06-11 23:36:45 +0200

    Add files via upload

---
+++ Day 8 - Function Parameters & Caesar Cipher/day-8-caesar-cipher-3/main.py
@@ -0,0 +1,27 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+
+def caesar(text, shift, direction):
+  final_text = ""
+  for letter in text:
+    position = alphabet.index(letter)
+    
+    if direction == "encode":
+      new_position = position + shift
+    else:
+      new_position = position - shift
+      
+    final_text += alphabet[new_position]
+  if direction == "encode":
+    print(f"The encoded text is {final_text}")
+  else:
+    print(f"The decoded text is {final_text}")
+
+caesar(text, shift, direction)
